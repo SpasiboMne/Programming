e6591483047618bba6449af698861ba1e5181b2e
SpasiboMne <agashev@gmail.com>
2024-10-02 17:19:11 +0300

    игого

--- Rust/project/guessing_game/src/main.rs
+++ Rust/project/guessing_game/src/main.rs
@@ -7,15 +7,8 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    // --snip--
-
-    // println!("The secret number is: {secret_number}");
-
     loop {
         println!("Please input your guess.");
-
-        // --snip--
-
 
         let mut guess = String::new();
 
